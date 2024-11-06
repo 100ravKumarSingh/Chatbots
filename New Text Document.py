925260a6d0ec34e531d41ca3eb515d902809b183
Saurav Kumar Singh <44571606+100ravKumarSingh@users.noreply.github.com>
2024-11-06 06:54:39 +0530

    Update and rename New Text Document.txt to New Text Document.py

---
+++ New Text Document.py
@@ -0,0 +1,56 @@
+import openai
+import os
+from dotenv import load_dotenv,find_dotenv
+_=load_dotenv(find_dotenv())
+openai.api_key=os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt,model='gpt-3.5-turbo'):
+    messages=[{"role":"user","content": prompt}]
+    response=openai.ChatCompletion.create(
+    model=model,
+    messages=messages,
+    temperature=0,
+    )
+    return response.choices[0].message["content"]
+    
+review=f"""Best place to plan your stay.\
+Good facilities available at affordable prices. The attached washroom in every room  is an\
+advantage.\
+The location is  quiet , reachable and   close to the market.\
+The manager is  polite .\
+Highly recommended."""
+
+prompt=f"""what is the sentiment of the review delimited by triple backticks.\
+Review : ```{review}```
+"""  
+sentiment=get_completion(prompt)
+
+prompt=f"""You are a customer support AI assistant.\
+Your task is to analyse the customer review and generate a reply based on the sentiment.\
+Analyse the review delimited by  ``` and create a response. If the sentiment given in <> is positive,\
+thank the customer. If it is negetive, apologise for the inconvinience and suggest that they can reach out to customer care.\
+Make sure to use specefic details from the review. Write in a concise and professional tone.\
+Review:```{review}``` \
+sentiment: <{sentiment}>"""
+
+response=get_completion(prompt)
+print(response) 
+ 
+# display panel 
+import panel as pn
+pn.extension()
+panels=[]
+context=[]
+input=pn.widgets.TextInput(value="Hi",placeholder="Enter your text here")
+button=pn.widgets.Button(name="Chat")
+interaction=pn.bind(collect_messages, button)
+# dashboard
+dashboard=pn.column(
+	inp,
+	pn.Row(button),
+	pn.panel(interaction,loading_indicator="true",height=300)
+)
+dashboard
+	
+   
+    
