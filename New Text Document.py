5b66a4e3016060cb8a243a37ef5b0506551aabc3
Saurav Kumar Singh <44571606+100ravKumarSingh@users.noreply.github.com>
2024-11-09 08:15:35 +0530

    Update New Text Document.py

--- New Text Document.py
+++ New Text Document.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv,find_dotenv
 _=load_dotenv(find_dotenv())
 openai.api_key=os.getenv('OPENAI_API_KEY')
 
+#get_completion----
 def get_completion(prompt,model='gpt-3.5-turbo'):
     messages=[{"role":"user","content": prompt}]
     response=openai.ChatCompletion.create(
@@ -22,59 +23,43 @@ def get_completion_from_messages(messages,model='gpt-3.5-turbo'):
     )
     return response.choices[0].message["content"]
     
-review=f"""Best place to plan your stay.\
-Good facilities available at affordable prices. The attached washroom in every room  is an\
-advantage.\
-The location is  quiet , reachable and   close to the market.\
-The manager is  polite .\
-Highly recommended."""
 
-prompt=f"""what is the sentiment of the review delimited by triple backticks.\
-Review : ```{review}```
-"""  
-sentiment=get_completion(prompt)
+#system instruction-------
+instruction=f"""You are a customer support AI assistant, an automated service to collect opinion of customer.\
+You first greet the customer, ask for opinion and analyse the customer's review and determine the sentiment of the review. \
+Generate a reply based on the sentiment.\ If the sentiment is positive,\thank the customer. If it is negetive, apologise for\
+the inconvinience and suggest that they can reach out to customer care.\Make sure to use specefic details from the review. \
+Write in a concise and professional tone."""
 
-prompt=f"""You are a customer support AI assistant.\
-Your task is to analyse the customer review and generate a reply based on the sentiment.\
-Analyse the review delimited by  ``` and create a response. If the sentiment given in <> is positive,\
-thank the customer. If it is negetive, apologise for the inconvinience and suggest that they can reach out to customer care.\
-Make sure to use specefic details from the review. Write in a concise and professional tone.\
-Review:```{review}``` \
-sentiment: <{sentiment}>"""
-
-response=get_completion(prompt)
-print(response) 
+context=[{'role':'system','content':f'{instruction}'}]
     
 
 #collect_messages
 def collect_messages(_):
     prompt=inp.value_input
-	inp.value=''
+    inp.value=''
     context.append({'role':'user','content':f"{prompt}"})
     response=get_completion_from_messages(context)
     panels.append(
-        pn.Row('user:', pn.pane.Markdown(prompt, width="300"))
+        pn.Row('user:', pn.pane.Markdown(prompt, width=300))
     )
     panels.append(
-        pn.Row('assistant:', pn.pane.Markdown(response, width="300"))
+        pn.Row('assistant:', pn.pane.Markdown(response, width=300))
     )
-    return pn.column(*panels)
+    return pn.Column(*panels)
 
 # display panel 
 import panel as pn
 pn.extension()
 panels=[]
 context=[]
-inp=pn.widgets.TextInput(value="Hi",placeholder="Enter your text here")
+inp=pn.widgets.TextInput(value="Hi",placeholder="Enter your text here",height=100)
 button=pn.widgets.Button(name="Chat")
 interaction=pn.bind(collect_messages, button)
 # dashboard
-dashboard=pn.column(
+dashboard=pn.Column(
 	inp,
 	pn.Row(button),
-	pn.panel(interaction,loading_indicator="true",height=300)
+	pn.panel(interaction,loading_indicator=True,height=300)
 )
 dashboard
-	
-   
-    
